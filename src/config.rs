62b5cd2b6e90aa2ca3a13fd4b67a4ff5691699a7
s;rx <8214028+wlrsx@users.noreply.github.com>
2025-03-13 22:59:42 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-hk.jadelive.top"];
+pub const RS_PUB_KEY: &str = "vY3zb8AiXZcxkSKw2N8IETxVuV4mQv3rqzrzDL53k1w=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
